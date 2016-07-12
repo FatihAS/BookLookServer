47324d88cd83e2e4c7c4f44a2f3a1b94b7263fb5
isdzulqor <midzulqornain@gmail.com>
2016-07-12 11:20:35 +0700

    update

--- client/views/loginAdmin.js
+++ client/views/loginAdmin.js
@@ -2,6 +2,3 @@ Template.loginAdmin.events({
     'click #cekAdmin': function (e, tmpl) {
         e.preventDefault();
         var email = tmpl.find('#email').value;
-        var pass = tmpl.find('#password').value;
-        Session.set('adminEmailSess', email);
-        console.log(Session.get('adminEmailSess'));
